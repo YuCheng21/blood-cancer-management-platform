554cae18da142d720f1402b5aae805d0bdfb4b1b
YuCheng <fyc621@gmail.com>
2022-02-19 15:29:23 +0800

    adjust: api #20220219002

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\CaseModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::post('/login', function (Request $request){
+    $input = [
+        'account' => ['required'],
+        'password' => ['required'],
+    ];
+    $validator = Validator::make($request->all(), $input);
+
+    if ($validator->fails()) {
+        return response(
+            json_encode([], JSON_FORCE_OBJECT),
+            401
+        );
+    }
+
+    $case = CaseModel::where('account', $request->account)->first();
+    if (is_null($case)){
+        return response(
+            json_encode([], JSON_FORCE_OBJECT),
+            401
+        );
+    }
+
+    return response(
+        json_encode($case, JSON_FORCE_OBJECT|JSON_UNESCAPED_UNICODE),
+        200
+    );
 });
