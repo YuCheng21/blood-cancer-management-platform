82aa7d6a51f507779dbf6f50d5da592bfa707001
YuCheng <fyc621@gmail.com>
2022-02-19 15:36:48 +0800

    adjust: CaseController.php #20220219003

--- app/Http/Controllers/CaseController.php
+++ app/Http/Controllers/CaseController.php
@@ -101,45 +101,51 @@ class CaseController extends Controller
     }
 
     public function update(Request $request, $account){
-        $case = CaseModel::where('account', $account)->first();
-        if (!is_null($case)){
-            $input = [
-                'updateCasePassword' => ['required'],
-                'updateCaseTransplantNum' => ['required'],
-                'updateCaseName' => ['required'],
-                'updateCaseGender' => ['required', 'in:男性,女性'],
-                'updateCaseBirth' => ['required'],
-                'updateCaseDate' => ['required'],
-                'updateCaseTransplantType' => ['required', 'in:自體移植,異體移植'],
-                'updateCaseDiseaseType' => ['required', 'in:AML,ALL,MM,何杰金氏淋巴癌,非何杰金氏淋巴癌'],
-                'updateCaseDiseaseState' => ['required'],
-                'updateCaseDiseaseClass' => ['required'],
-            ];
-            $validator = Validator::make($request->all(), $input);
-
-            if ($validator->fails()) {
-                return redirect()
-                    ->route('cases.index')
-                    ->with([
-                        'type' => 'error',
-                        'msg' => '表單填寫未完成'
-                    ]);
-            }
-
-            $data = [
-                'password' => $request->updateCasePassword,
-                'transplant_num' => $request->updateCaseTransplantNum,
-                'name' => $request->updateCaseName,
-                'gender_id' => Gender::where('name', $request->updateCaseGender)->first()->id,
-                'birthday' => $request->updateCaseBirth,
-                'date' => $request->updateCaseDate,
-                'transplant_type_id' => TransplantType::where('name', $request->updateCaseTransplantType)->first()->id,
-                'disease_type_id' => DiseaseType::where('name', $request->updateCaseDiseaseType)->first()->id,
-                'disease_state_id' => DiseaseState::where('name', $request->updateCaseDiseaseState)->first()->id,
-                'disease_class_id' => DiseaseClass::where('name', $request->updateCaseDiseaseClass)->first()->id,
-            ];
-            $case->update($data);
+        // Validated input
+        $input = [
+            'updateCasePassword' => ['required'],
+            'updateCaseTransplantNum' => ['required'],
+            'updateCaseName' => ['required'],
+            'updateCaseGender' => ['required', 'in:男性,女性'],
+            'updateCaseBirth' => ['required'],
+            'updateCaseDate' => ['required'],
+            'updateCaseTransplantType' => ['required', 'in:自體移植,異體移植'],
+            'updateCaseDiseaseType' => ['required', 'in:AML,ALL,MM,何杰金氏淋巴癌,非何杰金氏淋巴癌'],
+            'updateCaseDiseaseState' => ['required'],
+            'updateCaseDiseaseClass' => ['required'],
+        ];
+        $validator = Validator::make($request->all(), $input);
+        if ($validator->fails()) {
+            return redirect()
+                ->route('cases.index')
+                ->with([
+                    'type' => 'error',
+                    'msg' => '表單填寫未完成'
+                ]);
         }
+        // Query database
+        $case = CaseModel::where('account', $account)->first();
+        if (is_null($case)){
+            return redirect()
+                ->route('cases.index')
+                ->with([
+                    'type' => 'error',
+                    'msg' => '內容錯誤'
+                ]);
+        }
+        $data = [
+            'password' => $request->updateCasePassword,
+            'transplant_num' => $request->updateCaseTransplantNum,
+            'name' => $request->updateCaseName,
+            'gender_id' => Gender::where('name', $request->updateCaseGender)->first()->id,
+            'birthday' => $request->updateCaseBirth,
+            'date' => $request->updateCaseDate,
+            'transplant_type_id' => TransplantType::where('name', $request->updateCaseTransplantType)->first()->id,
+            'disease_type_id' => DiseaseType::where('name', $request->updateCaseDiseaseType)->first()->id,
+            'disease_state_id' => DiseaseState::where('name', $request->updateCaseDiseaseState)->first()->id,
+            'disease_class_id' => DiseaseClass::where('name', $request->updateCaseDiseaseClass)->first()->id,
+        ];
+        $case->update($data);
         return redirect()
             ->route('cases.index')
             ->with([
