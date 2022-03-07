273b0c3d67592a19743fd2a8a4af3ea113986fd1
YuCheng <fyc621@gmail.com>
2022-03-07 18:35:02 +0800

    adjust: testing #20220307004

--- app/Exports/TotalExport.php
+++ app/Exports/TotalExport.php
@@ -3,55 +3,28 @@
 namespace App\Exports;
 
 use App\Models\CaseModel;
-use Maatwebsite\Excel\Concerns\FromArray;
-use Maatwebsite\Excel\Concerns\WithHeadings;
-use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
-class TotalExport implements FromArray, WithTitle, WithHeadings
+class TotalExport implements WithMultipleSheets
 {
     protected $accounts;
 
-    public function __construct(array $accounts)
+    public function __construct($accounts)
     {
         $this->accounts = $accounts;
     }
 
-    public function array(): array
+    public function sheets(): array
     {
-        $cases = CaseModel::whereIn('id', $this->accounts)->get();
-        $cases = $cases->map(function ($case) {
-            return [
-                $case->account,
-                $case->password,
-                $case->transplant_num,
-                $case->name,
-                $case->gender->name,
-                $case->birthday,
-                $case->date,
-                $case->transplant_type->name,
-                $case->disease_type->name . ' - ' . $case->disease_state->name . ' - ' . $case->disease_class->name,
-            ];
-        });
-        return $cases->toArray();
-    }
+        $sheets = [];
 
-    public function headings(): array
-    {
-        return [
-            '帳號',
-            '密碼',
-            '移植編號',
-            '姓名',
-            '性別',
-            '生日',
-            '移植日期',
-            '移植種類',
-            '疾病種類',
-        ];
-    }
+//        $sheets[] = new CaseInformationExport($this->account);
+//        $sheets[] = new CaseBloodExport($this->account);
+//        $sheets[] = new CaseTaskExport($this->account);
+//        $sheets[] = new CaseMedicineExport($this->account);
+//        $sheets[] = new CaseEffectExport($this->account);
+//        $sheets[] = new CaseReportExport($this->account);
 
-    public function title(): string
-    {
-        return '個人資料';
+        return $sheets;
     }
 }
