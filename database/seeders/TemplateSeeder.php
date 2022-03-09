2e6c092d5768b56310dc5f2284f07d152b5331a8
YuCheng <fyc621@gmail.com>
2022-03-09 12:33:09 +0800

    add: C&D Template #20220309004

--- database/seeders/TemplateSeeder.php
+++ database/seeders/TemplateSeeder.php
@@ -19,148 +19,420 @@ class TemplateSeeder extends Seeder
 //            ->create();
 
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 3,
             'week' => 1,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 18,
             'week' => 2,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 19,
             'week' => 2,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 20,
             'week' => 2,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 21,
             'week' => 2,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 22,
             'week' => 2,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 23,
             'week' => 2,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 24,
             'week' => 2,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 25,
             'week' => 2,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 26,
             'week' => 2,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 27,
             'week' => 2,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 28,
             'week' => 2,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 29,
             'week' => 2,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 30,
             'week' => 2,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 31,
             'week' => 2,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 32,
             'week' => 2,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 76,
             'week' => 2,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 73,
             'week' => 3,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 74,
             'week' => 3,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 75,
             'week' => 3,
         ]);
         Template::create([
-            'name' => 'B',
-            'task_id' => 76,
-            'week' => 3,
-        ]);
-        Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 77,
             'week' => 3,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 78,
             'week' => 3,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 79,
             'week' => 3,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 80,
             'week' => 3,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 81,
             'week' => 3,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 82,
             'week' => 3,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 83,
             'week' => 3,
         ]);
         Template::create([
-            'name' => 'B',
+            'name' => 'B模板',
             'task_id' => 84,
+            'week' => 3,
+        ]);
+
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 3,
+            'week' => 1,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 18,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 19,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 20,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 21,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 22,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 23,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 24,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 25,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 26,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 27,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 28,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 29,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 30,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 31,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 32,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 73,
+            'week' => 3,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 74,
+            'week' => 3,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 75,
+            'week' => 3,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 77,
+            'week' => 3,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 78,
+            'week' => 3,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 79,
+            'week' => 3,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 80,
+            'week' => 3,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 81,
+            'week' => 3,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 82,
+            'week' => 3,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 83,
+            'week' => 3,
+        ]);
+        Template::create([
+            'name' => 'C模板',
+            'task_id' => 84,
+            'week' => 3,
+        ]);
+
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 3,
+            'week' => 1,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 11,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 12,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 13,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 14,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 15,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 16,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 17,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 18,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 19,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 20,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 21,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 22,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 23,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 24,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 25,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 26,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 27,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 28,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 29,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 30,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 31,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 32,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 76,
+            'week' => 2,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 73,
+            'week' => 3,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 74,
+            'week' => 3,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 75,
+            'week' => 3,
+        ]);
+        Template::create([
+            'name' => 'D模板',
+            'task_id' => 77,
             'week' => 3,
         ]);
     }
